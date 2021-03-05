b0b4abc3e121687070c121087c64aeab0ef11b86
jameswood <james_s_wood@outlook.com>
2021-03-05 13:19:52 -0500

    Adding files in project 'CreativeSolutions' to Git...

---
+++ CreativeSolutions/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CreativeSolutions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Hello Creative Solutions");
+            Console.WriteLine("This is so cool.");
+        }   
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
